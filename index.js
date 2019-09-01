878fbaa5629d4859bb75a91e4dadd27ebd03c2f1
sa77irak <sa77irak@gmail.com>
2019-09-01 12:06:13 +0900

    changed some comments

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const http = require('http');
 const url = require('url');
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+//console.log(json);
 const laptopData = JSON.parse(json);
+//console.log(laptopData);
 
 const server = http.createServer((req, res) => {
 
     const pathName = url.parse(req.url, true).pathname;
+    console.log(url.parse(req.url, true));
     const id = url.parse(req.url, true).query.id;
 
     if (pathName === '/products' || pathName === '/') {
